cdce0237996ed261699fee409897d053a9f67685
Robert Erady <137341984+robitiii@users.noreply.github.com>
2025-11-19 15:31:32 +0200

    exporting RecommendedCarousel from file.

--- src/components/Podcasts/index.js
+++ src/components/Podcasts/index.js
@@ -1,3 +1,4 @@
 export { default as PodcastGrid } from "./PodcastGrid";
 export { default as PodcastCard } from "./PodcastCard";
 export { default as PodcastDetail } from "./PodcastDetail";
+export { default as RecommendedCarousel } from "./RecommendedCarousel";
